0fafb688634c958b9c11c1d72c92797c3a322869
Clark David <clarkcatle8@gmail.com>
2025-08-27 22:52:45 +0800

    fix crop pages

--- app/(roles)/agriculturist/crops/layout.tsx
+++ app/(roles)/agriculturist/crops/layout.tsx
@@ -1,11 +1,14 @@
-import { NavbarComponent } from "@/component/server_component/navbarComponent";
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="min-h-screen flex">
-      <NavbarComponent />
-      <main className="flex-1 p-8">{children}</main>
+    <div>
+      <div className=" w-full bg-red-300 text-red-950 flex justify-center items-center py-4">
+        On production
+      </div>
+      {children}
     </div>
   );
 }
